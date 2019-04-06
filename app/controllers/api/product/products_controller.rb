2f6402dd02cf65cfab4a5b17babe47d7071f5536
Hector Andres Acosta Pozo <hector.acosta.pozo@outlook.com>
2019-04-06 18:18:24 -0400

    search products

--- app/controllers/api/product/products_controller.rb
+++ app/controllers/api/product/products_controller.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class Api::Product::ProductsController < ApplicationController
-  before_action :set_api_product_product, only: %i[show update destroy]
-  before_action :authenticate_user!, only: :create
+  before_action :set_api_product_product, only: %i[show update destroy append_images]
 
   # GET /api/product/products
   def index
@@ -16,21 +15,37 @@ class Api::Product::ProductsController < ApplicationController
     render json: @api_product_product
   end
 
-  # POST /api/product/products
+  api :GET, '/product/search', 'Retrieve a set of products that matches the terms sent in queries'
+  param :query, String, required: true, desc: 'Set of terms to find a product'
+  def query
+    render json: ::Product.search(params[:query]).map(&:sanitized_info), status: :ok
+  end
+
+  # POST /api/product
+  api :POST, '/product', 'Create a product as an admin'
+  param :product, Hash, required: true, desc: 'Product info' do
+    param :name, String, desc: 'Product name'
+    param :description, String, desc: 'Product description'
+    param :barcode, String, desc: 'Product barcode'
+    param :price, String, desc: 'Products price'
+    param :selling_dose, String, desc: 'Product selling dose'
+    param :selling_unit, String, desc: 'Product selling unit'
+  end
   def create
     @api_product_product = ::Product.new(api_product_product_params)
 
     if @api_product_product.save
-      render json: @api_product_product, status: :created, location: @api_product_product
+      render json: @api_product_product.sanitized_info, status: :created, location: @api_product_product
     else
       render json: @api_product_product.errors, status: :unprocessable_entity
     end
   end
 
+  api :POST, '/product/image', 'Appends an image to the images set'
+  param :image, File, required: true, desc: 'The image you want to upload'
   def append_images
-    if @api_product_product.pictures.attach(params[:image])
-
-    end
+    @api_product_product.pictures.attach(params[:image])
+    render json: @api_product_product.sanitized_info
   end
 
   # PATCH/PUT /api/product/products/1
@@ -56,6 +71,6 @@ class Api::Product::ProductsController < ApplicationController
 
   # Only allow a trusted parameter "white list" through.
   def api_product_product_params
-    params.fetch(:product, {})
+    params.require(:product).permit(:name, :description, :barcode, :price, :selling_dose, :selling_unit)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,30 @@
 class Product < ApplicationRecord
   has_many_attached :pictures
+  searchkick word_middle: [:product_name],
+             callbacks: :async
+
+  def search_data
+    {
+      id: id,
+      name: name,
+      description: description,
+      barcode: barcode,
+      price: price,
+      product_name: "#{description} #{name} #{barcode} #{price} #{id}"
+    }
+  end
+
+  def sanitized_info
+    {
+      id: id,
+      name: name,
+      description: description,
+      barcode: barcode,
+      price: price,
+      images: pictures.map do |picture|
+        rails_blob_path(picture, disposition: "attachment", only_path: true)
+      end
+    }
+  end
+
 end
